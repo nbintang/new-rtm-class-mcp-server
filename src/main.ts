387b66cb093a307b3785f22d2c9dfb0d35ae2b74
nbintang <nbintangh@gmail.com>
2026-03-19 16:09:26 +0700

    fix: change to express adapter

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter({ logger: true }),
-  );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   await app.listen(
     process.env.PORT ? Number(process.env.PORT) : 3000,
